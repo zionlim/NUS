09ac62de221367fa778bf9fdc399c57a2e3e71db
zionlim <139849003+zionlim@users.noreply.github.com>
2024-04-10 23:48:57 +0800

    Update db_connect.php

--- test/inc/db_connect.php
+++ test/inc/db_connect.php
@@ -1,8 +1,14 @@
 <?php
-	$conn = mysqli_connect("localhost", "root", "", "dg01");
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-	
+    $serverName = "swe5006svr.mysql.database.azure.com"; // Update with your Azure MySQL server name
+    $username = "swe5006"; // Update with your Azure MySQL username
+    $password = "P@ssw0rd"; // Update with your Azure MySQL password
+    $databaseName = "dg01"; // Update with your Azure MySQL database name
 
+    // Establishing the connection
+    $conn = new mysqli($serverName, $username, $password, $databaseName);
+
+    // Checking the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
